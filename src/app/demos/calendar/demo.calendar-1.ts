6d3ec49d3de5f6a518b9814c8c13b76164863862
Saeid Eid <saeideid@Saeids-MacBook-Pro.local>
2017-06-30 11:21:28 +0300

    Commenting Calendar Demo for Plunker

--- src/app/demos/calendar/demo.calendar-1.ts
+++ src/app/demos/calendar/demo.calendar-1.ts
@@ -2,11 +2,11 @@ import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/cor
 import { Themes } from 'cedrus-fusion';
 
 import { InputModel } from 'cedrus-fusion';
-import { CalendarModel } from 'cedrus-fusion';
+//import { CalendarModel } from 'cedrus-fusion';
 import { ButtonModel } from 'cedrus-fusion';
 
-import { CfCalendarComponent } from 'cedrus-fusion';
-import { CfCalendarService } from 'cedrus-fusion';
+//import { CfCalendarComponent } from 'cedrus-fusion';
+//import { CfCalendarService } from 'cedrus-fusion';
 
 @Component({
 	moduleId: module.id,
@@ -15,35 +15,35 @@ import { CfCalendarService } from 'cedrus-fusion';
 	styleUrls: ['./demo.calendar-1.scss']
 })
 export class CfDemoCalendar1 implements OnInit, AfterViewInit {
-	@ViewChild('myCalendar') cfCalendar: CfCalendarComponent;
+// 	@ViewChild('myCalendar') cfCalendar: CfCalendarComponent;
 
-	calendarOptions: CalendarModel = new CalendarModel({
-		header: {
-			left: 'prev,next today',
-			center: 'title',
-			right: 'month,agendaWeek,agendaDay'
-		},
-		defaultView: 'month',
-		display: true,
-		disable: false,
-    fixedWeekCount: false,
-    editable: true,
-    eventLimit: true, // allow "more" link when too many events
-    events: [],
-		googleCalendar: {
-			clientID: '429731391979-mnlhvot9rcsju9gnj1rtthopeplhag07.apps.googleusercontent.com',
-			calendarId: 'mikhail@interactive-clouds.com'
-		}
-  });
+// 	calendarOptions: CalendarModel = new CalendarModel({
+// 		header: {
+// 			left: 'prev,next today',
+// 			center: 'title',
+// 			right: 'month,agendaWeek,agendaDay'
+// 		},
+// 		defaultView: 'month',
+// 		display: true,
+// 		disable: false,
+//     fixedWeekCount: false,
+//     editable: true,
+//     eventLimit: true, // allow "more" link when too many events
+//     events: [],
+// 		googleCalendar: {
+// 			clientID: '429731391979-mnlhvot9rcsju9gnj1rtthopeplhag07.apps.googleusercontent.com',
+// 			calendarId: 'mikhail@interactive-clouds.com'
+// 		}
+//   });
 
-	constructor(public cfCalendarService: CfCalendarService) { }
+	// constructor(public cfCalendarService: CfCalendarService) { }
 
 	ngOnInit() {
 
 	}
 
 	ngAfterViewInit() {
-		this.cfCalendarService.setCalendarOptions(this.cfCalendar, this.calendarOptions);
-		this.cfCalendarService.getGoogleCalendarEvents();
+		// this.cfCalendarService.setCalendarOptions(this.cfCalendar, this.calendarOptions);
+		// this.cfCalendarService.getGoogleCalendarEvents();
 	}
 }
